f492b764ecc0414c55548c4a5716482a86eec1b5
Fabrice Normandin <fabrice.normandin@gmail.com>
2021-05-18 18:39:35 -0400

    Remove old version of 'gym-extensions-mod'
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>
    
    Fix setup.py and reuse original code
    
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>
    
    Remove all __pycache__ files from vcs
    
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>
    
    Unpin `gym` requirement
    
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- setup.py
+++ setup.py
@@ -7,14 +7,21 @@ print("Installing mjrl. \n Package intended for use with provided conda env. See
 if sys.version_info.major != 3:
     print("This Python is only compatible with Python 3, but you are running "
           "Python {}. The installation will likely fail.".format(sys.version_info.major))
-    
+
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(
     name='mjrl',
     version='1.0.0',
     packages=find_packages(),
+    install_requires=[
+        # "gym==0.10.9",
+        "gym_extensions @ git+https://www.github.com/lebrice/gym-extensions.git#egg=gym_extensions",
+        "metaworld @ git+https://github.com/rlworkgroup/metaworld.git@master#egg=metaworld",
+    ],
     description='RL algorithms for environments in MuJoCo',
     long_description=read('README.md'),
     url='https://github.com/aravindr93/mjrl.git',
